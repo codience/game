95767e7d8aaa876b943b4da4eeb14aaecf70469e
梅宮 <euthanasia045@gmail.com>
2019-06-08 23:00:06 +0900

    20190608

--- pyxel/test.py
+++ pyxel/test.py
@@ -11,6 +11,7 @@ class App:
     def __init__(self):
         pyxel.init(window_W,window_H,caption = "Hello pyxel")
         imgs = pyxel.load('./test.pyxel')
+        pyxel.mouse(True)
         pyxel.run(self.update,self.draw)
     
     def update(self):
@@ -19,8 +20,16 @@ class App:
     
     def draw(self):
         pyxel.cls(0)
-        pyxel.blt(0,0,0,0,0,16,16)
-        pyxel.blt(75,45,1,0,0,cat_W,cat_H,5)
+        pyxel.text(55,40,"codience",pyxel.frame_count % 16)
+        pyxel.blt(0,0,0,0,0,0,38,16)
+        self.cat()
 
+    def cat(self):
+        x = pyxel.mouse_x
+        y = pyxel.mouse_y
+        if pyxel.btn(pyxel.MOUSE_LEFT_BUTTON):
+            pyxel.blt(x,y,0,0,0,-cat_W,cat_H,5)
+        else:
+            pyxel.blt(x,y,0,0,0,cat_W,cat_H,5)
 
 App()
