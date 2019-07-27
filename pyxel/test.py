4b42510cd91b49c6cd8e1e6362c8a28575643a8f
梅宮 <euthanasia045@gmail.com>
2019-07-27 20:01:29 +0900

    add 0727

--- pyxel/test.py
+++ pyxel/test.py
@@ -2,6 +2,8 @@ import numpy as np
 import pyxel
 from PIL import Image as PilImage
 
+import pprint
+
 window_H = 120
 window_W = 160
 cat_H = 16
@@ -33,8 +35,8 @@ class Ball:
         self.color = 10
 
     def update(self,x,y,dx,size,color):
-        self.pos_x = x
-        self.pos_y = y
+        self.pos.x = x
+        self.pos.y = y
         self.vec = dx
         self.size = size
         self.color = color
@@ -44,8 +46,8 @@ class App:
     def __init__(self):
         self.IMG_ID0_X = 60
         self.IMG_ID0_y = 65
-        self.IMG_ID0 = 0
-        self.IMG_ID1 = 1
+        self.IMG_ID0 = 1
+        self.IMG_ID1 = 0
 
         pyxel.init(window_W,window_H,caption = "Hello pyxel")
         #imgs = pyxel.load('./test.pyxel')
@@ -62,30 +64,33 @@ class App:
     
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
-            pyxel.puit()
+            pyxel.quit()
 
         dx = pyxel.mouse_x - self.mcat.pos.x
-        dy = pyxel.mouse.y - self.mcat.pos.y
+        dy = pyxel.mouse_y - self.mcat.pos.y
         if dx != 0:
             self.mcat.update(pyxel.mouse_x,pyxel.mouse_y,dx)
         elif dy != 0:
-            self.mcat.update(pyxel.mouse_x,pyxel.mouse.y,self.mcat.vec)
+            self.mcat.update(pyxel.mouse_x,pyxel.mouse_y,self.mcat.vec)
         
         if pyxel.btnp(pyxel.MOUSE_LEFT_BUTTON):
             new_ball = Ball()
+            print('init new ball.')
+            print(new_ball.size)
             if self.mcat.vec > 0:
-                new_ball.update(self.mcat.pos.x + CAT_W / 2 + 6,
-                                self.mcat.pos.y + CAT_H / 2,
+                new_ball.update(self.mcat.pos.x + cat_W / 2 + 6,
+                                self.mcat.pos.y + cat_H / 2,
                                 self.mcat.vec, new_ball.size, new_ball.color)
             else:
-                new_ball.update(self.mcat.pos.x + CAT_W / 2 - 6,
-                                self.mcat.pos.y + CAT_H / 2,
+                new_ball.update(self.mcat.pos.x + cat_W / 2 - 6,
+                                self.mcat.pos.y + cat_H / 2,
                                 self.mcat.vec, new_ball.size, new_ball.color)
             self.Balls.append(new_ball)
+            pprint.pprint(self.Balls)
 
         ball_count = len(self.Balls)
         for i in range(ball_count):
-            if 0 < self.Balls[i].pos.x and self.Balls[i].pos.x < WINDOW_W:
+            if 0 < self.Balls[i].pos.x and self.Balls[i].pos.x < window_W:
                 if self.Balls[i].vec > 0:
                     self.Balls[i].update(self.Balls[i].pos.x + self.Balls[i].speed, 
                                         self.Balls[i].pos.y, 
@@ -103,14 +108,21 @@ class App:
         pyxel.cls(0)
         pyxel.text(55,40,"codience",pyxel.frame_count % 16)
         pyxel.blt(0,0,0,0,0,0,38,16)
-        self.cat()
 
-    def cat(self):
-        x = pyxel.mouse_x
-        y = pyxel.mouse_y
-        if pyxel.btn(pyxel.MOUSE_LEFT_BUTTON):
-            pyxel.blt(x,y,0,0,0,-cat_W,cat_H,5)
+        if self.mcat.vec > 0:
+            pyxel.blt(self.mcat.pos.x, self.mcat.pos.y, self.IMG_ID1, 0, 0, -cat_W, cat_H, 5)
         else:
-            pyxel.blt(x,y,0,0,0,cat_W,cat_H,5)
+            pyxel.blt(self.mcat.pos.x, self.mcat.pos.y, self.IMG_ID1, 0, 0, cat_W, cat_H, 5)
+ 
+        for ball in self.Balls:
+            pyxel.circ(ball.pos.x, ball.pos.y, ball.size, ball.color)
+
+    # def cat(self):
+    #     x = pyxel.mouse_x
+    #     y = pyxel.mouse_y
+    #     if pyxel.btn(pyxel.MOUSE_LEFT_BUTTON):
+    #         pyxel.blt(x,y,0,0,0,-cat_W,cat_H,5)
+    #     else:
+    #         pyxel.blt(x,y,0,0,0,cat_W,cat_H,5)
 
 App()
