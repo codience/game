920fbf4647e32aae63ff115eb8e06dcf8e600238
umemiya <umemiya@imac1.local>
2019-11-09 20:03:54 +0900

    1109 kouta

--- kouta/janken.py
+++ kouta/janken.py
@@ -1,57 +1,62 @@
 import random
 
 class okane:
-    def __init__(self, start=100, kakekin=20):
+     def __init__(self, start=100):
         self.money = start
-        self.kake = kakekin
-    def bet(self, kakekin, win):
-        if win == True:
-            self.money += kakekin
-        elif win == False:
-            self.money -= kakekin
-game = okane()
+     def bet(self, start, win):
+         if win == True:
+             self.money  *= 2
+         elif win == False:
+             self.money = 0
+     
+
+
 print("じゃんけんへようこそ！")
+start = int(input("スタート金額を入力してください"))
+game = okane(start=start)
+battle = int(input("試合回数を入力してください"))
 print("あなたの所持金は", game.money, "＄スタートです！")
 
-for _ in range(3):
+for _ in range(battle):
+
      hands = ["チョキ","グー","パー"]
      enemy_hand = hands[random.randint(0, 2)]
      my_hand = input("チョキ　グー　パー:")
      if my_hand == "チョキ":
           if enemy_hand == "チョキ":
                print("あいこです。")
-               print("引き分け！賭け金なし！"))
+               print("引き分け！賭け金なし！")
           elif enemy_hand == "グー":
                print("あなたの負けです。。。") 
                print("ザンネン！賭け金DOWN！")
-               game.bet(kakekin,False)
+               game.bet(start,False)
           elif enemy_hand == "パー":
                print("あなたの勝ちです！") 
                print("おめでとう！賭け金UP！") 
-               game.bet(kakekin,True) 
+               game.bet(start,True) 
      elif my_hand == "グー":
           if enemy_hand == "チョキ":
                     print("あなたの勝ちです。")
                     print("おめでとう！賭け金UP！")
-                    game.bet(kakekin,True)
+                    game.bet(start,True)
           elif enemy_hand == "グー":
                     print("あいこです。")
                     print("引き分け！賭け金なし！")
           elif enemy_hand == "パー":
                     print("あなたの負けです。。。")
                     print("ザンネン！賭け金DOWN！")
-                    game.bet(kakekin,False)
+                    game.bet(start,False)
      elif my_hand == "パー":
           if enemy_hand == "チョキ":
                     print("あなたの負けです。。。")
                     print("ザンネン！賭け金DOWN！")
-                    game.bet(kakekin,False)
+                    game.bet(start,False)
           elif enemy_hand == "グー":
                     print("あなたの勝ちです！")
                     print("おめでとう！賭け金UP！")
-                    game.bet(kakekin,True)
+                    game.bet(start,True)
           elif enemy_hand == "パー":
                     print("あいこです。")
-                    print("引き分け！賭け金なし！)) 
+                    print("引き分け！賭け金なし！")
      print("相手の数字は", enemy_hand,"でした！")
      print("あなたの所持金は", game.money,"$です！")
